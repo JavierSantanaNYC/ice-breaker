ce80793d99fc90ebcd422b8413683a2751c2dbef
JavierSantanaNYC <28825631+JavierSantanaNYC@users.noreply.github.com>
2023-04-23 22:37:39 -0400

    attendee selection

--- web/icebreaker/src/attendees.js
+++ web/icebreaker/src/attendees.js
@@ -1,7 +1,8 @@
 import attendees from "./attendee-categories.json";
-import React from 'react';
+import React from "react";
 
 const AttendeesPage = () => {
+  const [selected, setSelected] = React.useState([]);
   const categories = attendees.reduce((acc, attendee) => {
     if (!acc.includes(attendee.category)) {
       acc.push(attendee.category);
@@ -9,19 +10,35 @@ const AttendeesPage = () => {
     return acc;
   }, []);
 
+  const handleClick = (e) => {
+    if (!selected.has(e.target.value)) {
+      // Add item to selection
+      selected.add(e.target.value);
+    } else {
+      // Remove item from selection
+      selected.delete(e.target.value);
+    }
+  };
+
   return (
     <>
+      {selected.length === 2 ? (
+        <button>Compare</button>
+      ) : (
+        <p>select two attendees to view interaction question.</p>
+      )}
       {categories.map((category) => (
         <div>
           <h1>{category}</h1>
-          <ul>
-            {attendees.map((attendee) => {
-              if (attendee.category === category) {
-                return <li>{attendee.full_name}</li>;
-              }
-              return null;
-            })}
-          </ul>
+
+          {attendees.map((attendee) => {
+            if (attendee.category === category) {
+              return (
+                <select onClick={handleClick}>{attendee.full_name}</select>
+              );
+            }
+            return null;
+          })}
         </div>
       ))}
     </>
