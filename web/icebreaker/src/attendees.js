bed2fb40b07f78b30487a8cf403039b64be572a7
JavierSantanaNYC <28825631+JavierSantanaNYC@users.noreply.github.com>
2023-04-23 23:50:50 -0400

    attendee selection

--- web/icebreaker/src/attendees.js
+++ web/icebreaker/src/attendees.js
@@ -3,6 +3,11 @@ import React from "react";
 
 const AttendeesPage = () => {
   const [selected, setSelected] = React.useState([]);
+  const [checkboxes, setCheckboxes] = React.useState(
+    attendees.map((attendee) => {
+      return { id: attendee.id, checked: false, label: attendee.full_name, category: attendee.category };
+    })
+  );
   const categories = attendees.reduce((acc, attendee) => {
     if (!acc.includes(attendee.category)) {
       acc.push(attendee.category);
@@ -10,19 +15,34 @@ const AttendeesPage = () => {
     return acc;
   }, []);
 
-  const handleClick = (e) => {
-    if (!selected.has(e.target.value)) {
-      // Add item to selection
-      selected.add(e.target.value);
-    } else {
-      // Remove item from selection
-      selected.delete(e.target.value);
-    }
-  };
+  const handleCheckboxChange = React.useCallback((id) => {
+    setCheckboxes((prevState) => {
+      const newState = prevState.map((checkbox) => {
+        console.log(checkbox.id, id)
+        if (checkbox.id === id) {
+          // If the current checkbox is already checked, toggle it
+          if (checkbox.checked) {
+            return { ...checkbox, checked: !checkbox.checked };
+          }
+
+          // Count the number of checked checkboxes
+          const checkedCount = prevState.filter((cb) => cb.checked).length;
+
+          // If the limit of two checked checkboxes is not reached, toggle the current checkbox
+          if (checkedCount < 2) {
+            return{ ...checkbox, checked: !checkbox.checked };
+          }
+        }
+        return checkbox;
+      });
+
+      return newState;
+    });
+  }, []);
 
   return (
     <>
-      {selected.length === 2 ? (
+      {checkboxes.filter((cb) => cb.checked).length === 2 ? (
         <button>Compare</button>
       ) : (
         <p>select two attendees to view interaction question.</p>
@@ -31,14 +51,21 @@ const AttendeesPage = () => {
         <div>
           <h1>{category}</h1>
 
-          {attendees.map((attendee) => {
-            if (attendee.category === category) {
-              return (
-                <select onClick={handleClick}>{attendee.full_name}</select>
-              );
-            }
-            return null;
-          })}
+          {checkboxes.map((checkbox) => (
+            checkbox.category === category && (
+            <div key={checkbox.id}>
+              <input
+                type="checkbox"
+                id={`checkbox-${checkbox.id}`}
+                checked={checkbox.checked}
+                onChange={() => handleCheckboxChange(checkbox.id)}
+              />
+              <label htmlFor={`checkbox-${checkbox.id}`}>
+                {checkbox.label}
+              </label>
+            </div>
+            )
+          ))}
         </div>
       ))}
     </>
